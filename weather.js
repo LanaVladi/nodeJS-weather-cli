60604d83008c23c9d920b1247d794e5492767ec5
Svetlana Vladi <lanavladi05@gmail.com>
2024-07-28 12:22:11 +0500

    add saveCity and getForecast in weather.js

--- weather.js
+++ weather.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
-import { getWeather } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { getKeyValue, saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -18,22 +18,58 @@ const saveToken = async (token) => {
     }
 }
 
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError(`A city isn't passed`);
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('City is saved successfully!');
+    } catch (error) {
+        printError(`City is not saved: ${error.message}`);
+    }
+}
+
+const getForecast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+        const weather = await getWeather(city);
+        const weatherIcon = weather.weather[0].icon;
+        printWeather(weather, getIcon(weatherIcon));
+        // console.log("weather:", weather);
+    } catch (error) {
+        if (error?.response?.status === 404) {
+            printError(`${error.message}: city not found `);
+        } else if (error?.response?.status === 401) {
+            printError(`${error.message}: API-key is not correct `);
+        } else {
+            printError(`Forecast error: ${error.message}`);
+        }
+    }
+}
+
 function initCLI() {
     const commandLineArguments = process.argv;
     const args = getArgs(commandLineArguments);
+    // console.log("process.env:", process.env);
+
     if (args.h) {
         // Enter help
-        printHelp();
+        return printHelp();
     }
     if (args.s) {
         // Save city
+        return saveCity(args.s);
+        // getForecast(args.s)
+        // checkCity(args.s);
     }
     if (args.t) {
         // Save token
         return saveToken(args.t);
     }
 
-    getWeather('moscow');
+    return getForecast();
     //   Enter weather
 }
 
